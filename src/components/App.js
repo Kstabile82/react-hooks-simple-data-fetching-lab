07cebf88836098f9030a9ac00a3169b4d3a38b0e
KarinaStabile <karina.stabile@gmail.com>
2021-12-12 12:32:32 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,22 @@
-// create your App component here
+import React, {useState, useEffect} from "react";
+function App() {
+    const [dogpic, setDogPic] = useState(); 
+    const [isLoaded, setIsLoaded] = useState(false);
+useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then((response) => response.json())
+    .then((data)=> {
+        setDogPic(data.message)
+        setIsLoaded(true);
+    });
+}, []);
+if (!isLoaded) return <p>Loading...</p>
+return <img alt="A Random Dog." src= {dogpic} />
+// Use the useEffect hook in the App component. 
+    //Inside the callback for useEffect, send a fetch request to , a free API that returns a random image of a dog.
+// Display a <p> tag with the text of "Loading..." when the component is first rendered
+// After receiving a response from the API, show the dog image in an <img> tag, 
+    //with the alt attribute set to "A Random Dog".
+}
+
+export default App; 
